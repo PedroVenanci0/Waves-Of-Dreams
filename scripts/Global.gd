7f415411f209e9d731396be38c6b1f4dbea5bb41
Nicolas Rafael <nicolasalves136@gmail.com>
2024-05-21 12:07:09 -0300

    Adicionado funcionalidade das poções

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -3,15 +3,21 @@ extends Node
 var transition_scene: PackedScene = preload("res://scenes/fade_in_canvas_layer.tscn")
 var level_node: level
 var enemies_killed: int = 0
-var max_enemies = 20
-var enemies_spawn = 0
-var spawn_permission: bool = true
+
 var scenes_database: Dictionary = {
 	"title": preload("res://scenes/title_screen.tscn"),
 	"tavern": preload("res://scenes/tavern_scene.tscn"),
 	"cave": preload("res://scenes/cave_scene.tscn"),
 	"transition": null
 }
+
+@export_category("Spawner Variables")
+@export var max_enemies = 20
+@export var enemies_spawn = 0
+var spawn_permission: bool = true
+
+
+@export_category("Player Variables")
 @export var damage_player: int = 1
 @export var life_player: int = 3
 @export var move_speed: float = 128
@@ -21,3 +27,7 @@ func transitionToScene(destiny_scene: String) -> void:
 	var trans = transition_scene.instantiate()
 	trans.destiny_scene = scenes_database.get(destiny_scene) 
 	add_child(trans)
+
+func _process(delta):
+	if life_player > 5:
+		life_player = 5
