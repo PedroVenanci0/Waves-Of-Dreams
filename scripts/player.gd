edab72d97eb3ab2878037ed48b08c030b7893f27
Nicolas Rafael <nicolasalves136@gmail.com>
2024-05-21 09:09:23 -0300

    Alterações na Caverna

--- scripts/player.gd
+++ scripts/player.gd
@@ -51,11 +51,11 @@ func _move() -> void:
 		_animation_tree["parameters/walk/blend_position"] =  _direction
 		_animation_tree["parameters/attack/blend_position"] =  _direction
 		
-		velocity.x = lerp(velocity.x, _direction.normalized().x * _move_speed, _acceleration)
-		velocity.y = lerp(velocity.y, _direction.normalized().y * _move_speed, _acceleration)
+		velocity.x = lerp(velocity.x, _direction.normalized().x * move_speed, _acceleration)
+		velocity.y = lerp(velocity.y, _direction.normalized().y * move_speed, _acceleration)
 		return
 		
-    
+	
 	velocity.x = lerp(velocity.x, _direction.normalized().x * move_speed,_friction)
 	velocity.y = lerp(velocity.y, _direction.normalized().y * move_speed, _friction)
 	
